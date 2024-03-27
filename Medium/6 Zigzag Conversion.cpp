43021f4e072639a2b7e07fb0747ecc96c198867c
CJ <cjrodriguez2448@gmail.com>
2024-03-26 21:08:44 -0700

    minor changes

--- Medium/6 Zigzag Conversion.cpp
+++ Medium/6 Zigzag Conversion.cpp
@@ -7,20 +7,21 @@ using std::vector;
 class Solution {
 public:
     string convert(string s, int numRows) {
-        vector<string>v(numRows);
+        vector<string> v (numRows);
         bool flag=true;
+
         for(int i=0;i<s.size();){
-            if(flag){
+            if (flag) {
                 for(int j=0;j<numRows &&i<s.size();j++)
                 v[j].push_back(s[i++]);
             }
-            else{
-                for(int j=numRows-2;j>=1 && i<s.size();j--)v[j].push_back(s[i++]);
-            }
+            else
+                for (int j=numRows-2;j>=1 && i<s.size();j--)v[j].push_back(s[i++]);
             flag=!flag;
         }
+
         string ans="";
-        for(auto &x:v)ans+=x;
+        for(auto &x:v) ans+=x;
         return ans;
     }
 };
