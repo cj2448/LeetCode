2d6489799cb1f6cadf0bad8d8be4a3b76ebfbd80
CJ <cjrodriguez2448@gmail.com>
2024-04-29 13:25:53 -0700

    new commit

---
+++ Hard/10 Regular Expression Matching.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    bool isMatch(std::string s, std::string p) {
+        int n = s.size(), m = p.size();
+        std::vector<std::vector<bool>> dp(n + 1, std::vector<bool>(m + 1, false));
+        dp[0][0] = true;
+        
+        for(int i=0; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(p[j-1] == '*'){
+                    dp[i][j] = dp[i][j-2] || (i > 0 && (s[i-1] == p[j-2] || p[j-2] == '.') && dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = i > 0 && dp[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '.');
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
